4505198920ee5ac89011e7921a3488ffcc60d5d5
jayeesma <43064735+jayeesma@users.noreply.github.com>
2026-06-29 21:21:16 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        char[] cs = s.toCharArray();
+        char[] ct = t.toCharArray();
+        Arrays.sort(cs);
+        Arrays.sort(ct);
+
+        return Arrays.equals(ct,cs);
+
+        
+    }
+}
